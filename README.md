210e18ab75db95ecf32967243ab5eda7f8c4210b
Frank Brehm <frank@brehm-online.com>
2015-02-05 22:47:42 +0100

    Added .gitignore and geo_countries.py

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ geo_countries.py - 2-letter, 3-letter, and numerical codes for countries.
 
 ## Synopsis
 
-	from geo_counties import country, country_codes
-	from geo_counties import CNT_I_CODE2, CNT_I_CODE3
+	from geo_countries import country, country_codes
+	from geo_countries import CNT_I_CODE2, CNT_I_CODE3
 
 	my_country = country('DE')	# 'Germany'
 	codes = country_codes(666)	# ('PM', 'SPM', 666, 'Saint Pierre and Miquelon', 1)
---
+++ geo_countries.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Frank Brehm
+@contact: frank@brehm-online.com
+@copyright: © 2015 by Frank Brehm, Berlin
+@summary: 2-letter, 3-letter, and numerical codes for countries.
+"""
+
+# Standard modules
+
+
+
+
+__author__ = 'Frank Brehm <frank@brehm-online.com>'
+__copyright__ = '(C) 2015 by Frank Brehm, Berlin'
+__contact__ = 'frank@brehm-online.com'
+__version__ = '0.1.0'
+__license__ = 'LGPLv3+'
+
+
+# =============================================================================
+
+if __name__ == "__main__":
+
+    pass
+
+# =============================================================================
+
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
